ddefe8af21fed59b007f2f3da794429e087a339f
corner <sptdfdzl@163.com>
2022-11-20 14:27:01 +0800

    add: state和setState

--- packages/my-app/src/Hello.js
+++ packages/my-app/src/Hello.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
 class Hello extends React.Component {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     count: 0,
+  //   }
+  // }
+
+  state = {
+    count: 10,
+  }
   handleClick() {
     console.log('click事件触发了')
   }
   render() {
-    return <h1 onClick={this.handleClick}>抽离的Hello 类组件</h1>
+    return (
+      <div>
+        <h1 onClick={this.handleClick}>抽离的Hello 类组件</h1>
+        <p>count: {this.state.count}</p>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            })
+          }}
+        >
+          +1
+        </button>
+      </div>
+    )
   }
 }
 

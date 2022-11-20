cd78d8129f4b014a8605bdb02ed070bb11063509
corner <sptdfdzl@163.com>
2022-11-20 15:08:45 +0800

    add: 受控组件表单处理

--- packages/my-app/src/Hello.js
+++ packages/my-app/src/Hello.js
@@ -3,6 +3,7 @@ import React from 'react'
 class Hello extends React.Component {
   state = {
     count: 10,
+    txt: 'txt',
   }
   handleClick() {
     console.log(this)
@@ -13,12 +14,23 @@ class Hello extends React.Component {
       count: this.state.count + 1,
     })
   }
+
+  onInputChange = (e) => {
+    this.setState({
+      txt: e.target.value,
+    })
+  }
   render() {
     return (
       <div>
         <h1 onClick={() => this.handleClick()}>抽离的Hello 类组件</h1>
         <p>count: {this.state.count}</p>
         <button onClick={this.onIncrement}>+1</button>
+        <input
+          type="text"
+          value={this.state.txt}
+          onChange={this.onInputChange}
+        ></input>
       </div>
     )
   }

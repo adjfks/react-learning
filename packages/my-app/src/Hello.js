1956abdb4302c8cd509231b25758a21601c6d657
corner <sptdfdzl@163.com>
2022-11-20 14:16:40 +0800

    add: 事件处理及事件对象

--- packages/my-app/src/Hello.js
+++ packages/my-app/src/Hello.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 class Hello extends React.Component {
+  handleClick() {
+    console.log('click事件触发了')
+  }
   render() {
-    return <h1>抽离的Hello 类组件</h1>
+    return <h1 onClick={this.handleClick}>抽离的Hello 类组件</h1>
   }
 }
 
--- packages/my-app/src/index.js
+++ packages/my-app/src/index.js
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Hello from './Hello'
+
+const Hi = () => {
+  const handleClick = (e) => {
+    e.preventDefault()
+    console.log('hi')
+  }
+
+  return (
+    <a href="https://www.yuque.com/dashboard/books" onClick={handleClick}>
+      我是a链接
+    </a>
+  )
+}
 const main = (
   <div>
     <Hello />
+    <Hi />
   </div>
 )
 ReactDOM.render(main, document.getElementById('root'))

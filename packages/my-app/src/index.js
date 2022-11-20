2b6aabe4876b79adfad81d704a238ba38ee8546f
corner <sptdfdzl@163.com>
2022-11-20 10:59:52 +0800

    add: 条件渲染

--- packages/my-app/src/index.js
+++ packages/my-app/src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const title = <h1>Hello JSX</h1>
-ReactDOM.render(title, document.getElementById('root'))
+const name = 'Joke'
+const title = <h1>{name}</h1>
+
+const isLoding = false
+const getContent = () => {
+  return isLoding ? <div>loading</div> : <div>loaded</div>
+}
+
+const main = (
+  <div>
+    {title}
+    {getContent()}
+  </div>
+)
+ReactDOM.render(main, document.getElementById('root'))

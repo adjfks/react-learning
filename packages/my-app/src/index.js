d150fdddeafae65b6ecdcf9a9556f90df9e5632d
corner <sptdfdzl@163.com>
2022-11-20 11:04:53 +0800

    add:列表渲染

--- packages/my-app/src/index.js
+++ packages/my-app/src/index.js
@@ -15,4 +15,18 @@ const main = (
     {getContent()}
   </div>
 )
-ReactDOM.render(main, document.getElementById('root'))
+
+const songs = [
+  { id: 1, name: '痴心绝对' },
+  { id: 2, name: '像我这样的人' },
+  { id: 3, name: '南山南' },
+]
+const list = (
+  <ul>
+    {songs.map((item) => (
+      <li id={item.id}>{item.name}</li>
+    ))}
+  </ul>
+)
+
+ReactDOM.render(list, document.getElementById('root'))

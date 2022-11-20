d50dc8da17974561ae330399158d1fb15fd0a2b0
corner <sptdfdzl@163.com>
2022-11-20 14:45:37 +0800

    this绑定

--- packages/my-app/src/Hello.js
+++ packages/my-app/src/Hello.js
@@ -1,33 +1,24 @@
 import React from 'react'
 
 class Hello extends React.Component {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     count: 0,
-  //   }
-  // }
-
   state = {
     count: 10,
   }
   handleClick() {
+    console.log(this)
     console.log('click事件触发了')
+  }
+  onIncrement = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
   }
   render() {
     return (
       <div>
-        <h1 onClick={this.handleClick}>抽离的Hello 类组件</h1>
+        <h1 onClick={() => this.handleClick()}>抽离的Hello 类组件</h1>
         <p>count: {this.state.count}</p>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            })
-          }}
-        >
-          +1
-        </button>
+        <button onClick={this.onIncrement}>+1</button>
       </div>
     )
   }
